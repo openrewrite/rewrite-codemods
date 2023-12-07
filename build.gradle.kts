1dd7c5d3f993def4651d81bbaf5808a37033330f
Knut Wannheden <knut@moderne.io>
2023-12-07 12:30:20 +0100

    Fix build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.gradle.node.npm.task.NpmInstallTask
+
 plugins {
     id("org.openrewrite.build.recipe-library") version "latest.release"
     id("com.github.node-gradle.node") version "latest.release"
@@ -16,23 +18,71 @@ dependencies {
     testImplementation("org.openrewrite:rewrite-test")
 }
 
-afterEvaluate {
-    // Disable the sourcesJar and javadocJar from being published. The `plugin-publish-plugin` adds these in an
-    // `afterEvaluate` block, so we need to exclude them in one after that
-    val javaComponent = components["java"] as AdhocComponentWithVariants
-    javaComponent.withVariantsFromConfiguration(configurations["sourcesElements"]) {
-        skip()
-    }
-    javaComponent.withVariantsFromConfiguration(configurations["javadocElements"]) {
-        skip()
-    }
-}
-
 // We don't care about publishing javadocs anywhere, so don't waste time building them
 tasks.withType<Javadoc>().configureEach {
     enabled = false
 }
 
 node {
-    nodeProjectDir.set(file("src/main/resources/codemods"))
+    nodeProjectDir.set(file("build/resources/main/codemods"))
+}
+
+tasks.named("processResources") {
+    dependsOn(tasks.withType(NpmInstallTask::class.java))
+}
+
+tasks.named<Jar>("sourcesJar") {
+    enabled = false
+}
+
+tasks.named<Jar>("javadocJar") {
+    enabled = false
+}
+
+val emptySourceJar = tasks.create<Jar>("emptySourceJar") {
+    file("README.md")
+    archiveClassifier.set("sources")
+}
+
+val emptyJavadocJar = tasks.create<Jar>("emptyJavadocJar") {
+    file("README.md")
+    archiveClassifier.set("javadoc")
+}
+
+publishing {
+    publications.named<MavenPublication>("nebula") {
+        artifactId = project.name
+        description = project.description
+
+        artifacts.clear() // remove the regular JAR
+        // Empty JARs are OK: https://central.sonatype.org/publish/requirements/#supply-javadoc-and-sources
+        artifact(tasks.named("jar"))
+        artifact(emptySourceJar)
+        artifact(emptyJavadocJar)
+
+        pom {
+            name.set(project.name)
+            description.set(project.description)
+            url.set("https://moderne.io")
+            licenses {
+                license {
+                    name.set("Creative Commons Attribution-NonCommercial 4.0")
+                    url.set("https://creativecommons.org/licenses/by-nc-nd/4.0/deed.en")
+                }
+            }
+            developers {
+                developer {
+                    name.set("Team Moderne")
+                    email.set("support@moderne.io")
+                    organization.set("Moderne, Inc.")
+                    organizationUrl.set("https://moderne.io")
+                }
+            }
+            scm {
+                connection.set("scm:git:git://github.com/moderneinc/rewrite-codemods.git")
+                developerConnection.set("scm:git:ssh://github.com:moderneinc/rewrite-codemods.git")
+                url.set("https://github.com/moderneinc/rewrite-codemods/tree/main")
+            }
+        }
+    }
 }
