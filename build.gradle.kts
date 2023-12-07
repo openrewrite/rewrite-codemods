37d3aa543e4be73f73d71918516c49dbaaefe134
Knut Wannheden <knut@moderne.io>
2023-12-07 08:53:20 +0100

    Polish build

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 // Set as appropriate for your organization
 group = "org.openrewrite.recipe"
-description = "JavaScript codemods"
+description = "Migrate JavaScript projects using codemods"
 
 val rewriteVersion = rewriteRecipe.rewriteVersion.get()
 dependencies {
@@ -14,4 +14,21 @@ dependencies {
     implementation("org.openrewrite:rewrite-core")
 
     testImplementation("org.openrewrite:rewrite-test")
+}
+
+afterEvaluate {
+    // Disable the sourcesJar and javadocJar from being published. The `plugin-publish-plugin` adds these in an
+    // `afterEvaluate` block, so we need to exclude them in one after that
+    val javaComponent = components["java"] as AdhocComponentWithVariants
+    javaComponent.withVariantsFromConfiguration(configurations["sourcesElements"]) {
+        skip()
+    }
+    javaComponent.withVariantsFromConfiguration(configurations["javadocElements"]) {
+        skip()
+    }
+}
+
+// We don't care about publishing javadocs anywhere, so don't waste time building them
+tasks.withType<Javadoc>().configureEach {
+    enabled = false
 }
